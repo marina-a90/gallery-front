f03d0584fe5ad104b566a68f33979494ff65d265
marina-a90 <marinaandjelkovic456@gmail.com>
2019-04-15 22:00:01 +0200

    bootstrap i navbar dodati

---
+++ src/layout/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Gallery</h1>
+
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand class="navbar-brand">All Galleries</b-navbar-brand>
+            <b-navbar-brand class="navbar-brand">My Galleries</b-navbar-brand>
+            <b-navbar-brand class="navbar-brand">Create New Gallery</b-navbar-brand>
+            <b-navbar-brand class="navbar-brand">Register</b-navbar-brand>
+            <b-navbar-brand class="navbar-brand">Login</b-navbar-brand>
+            <b-navbar-brand class="navbar-brand">Logout</b-navbar-brand>
+        </b-navbar>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .h1 {
+
+    }
+</style>
