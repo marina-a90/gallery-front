f07b8785fa8f7d36073e9a11ee72e744d9283d48
marina-a90 <marinaandjelkovic456@gmail.com>
2019-04-16 12:39:28 +0200

    dodat vuex i prazan store

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { router } from './router'
+import Vuex from 'vuex';
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+Vue.use(Vuex)
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
